9bf0a933a3b28c325f8013a8b70a2de7cb3b2716
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2024-08-09 14:02:47 +0300

    Updated scripts

--- MangaInUaDownloader/pack.bat
+++
@@ -1,3 +0,0 @@
-dotnet pack -p:PackageID=manga.in.ua-downloader -c Release
-@echo off
-pause
--- MangaInUaDownloader/release.bat
+++ MangaInUaDownloader/release.bat
@@ -1,17 +1,18 @@
 @echo off
 
-REM LMAO I ChatGPTed it 0)))))
+powershell write-host -fore Yellow Building the project
+dotnet pack -p:PackageID=manga.in.ua-downloader -c Release
 
-echo Removing files from ./pack/nupkg/...
+powershell write-host -fore Yellow Removing files from ./pack/nupkg/...
 del /Q "pack\nupkg\*"
 
-echo Copying latest release from ./nupkg/ to ./pack/nupkg/...
+powershell write-host -fore Yellow Copying latest release from ./nupkg/ to ./pack/nupkg/...
 for /f %%i in ('dir /B /O:D nupkg\*') do set "latest=%%i"
 copy "nupkg\%latest%" "pack\nupkg\"
 
-echo Zipping latest release into ./Releases/...
+powershell write-host -fore Yellow Zipping latest release into ./Releases/...
 for %%F in ("pack\%latest%") do set "zipName=%%~nF"
 powershell Compress-Archive -Path "pack\*" -Force -DestinationPath "Releases\%zipName%.zip"
 
-echo Done.
+powershell write-host -fore Yellow Done.
 pause
